76d9b546f9795daac58db32ea67a59fb58e4d6ad
Sergey Nikulin <snikulin@gmail.com>
2024-07-27 23:07:29 +0300

    updated todo-keywords

--- config.el
+++ config.el
@@ -41,6 +41,9 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(setq org-todo-keywords '((sequence "TODO(t)" "IDEA(i)" "WAIT(w)" "HOLD(h)" "|" "DONE(d)" "CANC(c)")
+                          (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
+                          (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
